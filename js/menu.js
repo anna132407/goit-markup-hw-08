ef1c9382dfdd8b4c2d77c7e617c4c2cb50b5198f
anna132407 <anna132407@gmail.com>
2022-11-21 15:22:43 +0200

    правки, но не очень

--- js/menu.js
+++ js/menu.js
@@ -20,3 +20,18 @@
     refs.body.classList.remove('no-scroll');
   }
 })();
+
+// (() => {
+//   const menuBtnRef = document.querySelector("[data-menu-button]");
+//   //const mobileMenuRef = document.querySelector("[data-menu]");
+
+//   menuBtnRef.addEventListener("click", () => {
+//     // const expanded =
+//     //   menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+//     menuBtnRef.classList.toggle("is-open");
+//     // menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+//     // mobileMenuRef.classList.toggle("is-open");
+//   });
+// })();
